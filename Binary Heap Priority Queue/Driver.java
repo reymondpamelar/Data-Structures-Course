da2659afadfdea33fa7f9cee415c0d108549b225
reymondpamelar <56659292+reymondpamelar@users.noreply.github.com>
2020-08-03 13:35:47 -0700

    Create Driver.java

---
+++ Binary Heap Priority Queue/Driver.java
@@ -0,0 +1,48 @@
+// TestUnorderedArrayPriorityQueue class implementation
+package data_structures;
+import java.util.Iterator;
+public class Driver
+{
+   public static void main(String[] args)
+   {
+	   BinaryHeapPriorityQueue<Integer> upq = new BinaryHeapPriorityQueue<Integer>();
+      
+       upq.insert(5);
+       upq.insert(7);
+       upq.insert(4);
+       upq.insert(6);
+       upq.insert(2);
+       upq.insert(7);
+       upq.insert(7);
+       upq.insert(6);
+       
+      
+       System.out.println("Unordered PQ: ");      
+       Iterator<Integer> itr = upq.iterator();      
+       while(itr.hasNext())
+       {
+           System.out.println(itr.next());          
+       }
+      
+       System.out.println("\nsize: " + upq.size());
+       System.out.println("Removed : " + upq.remove()); 
+       System.out.println("delete: " + upq.delete(8));
+       System.out.println("delete: " + upq.delete(9));
+       System.out.println("delete: " + upq.delete(7));
+       System.out.println("Peek : " + upq.peek());
+       System.out.println("Contains : " + upq.contains(7));
+       System.out.println("empty" + upq.isEmpty());
+       
+       System.out.println("\n new Unordered PQ: ");
+       itr = upq.iterator();      
+       while(itr.hasNext())
+       {
+           System.out.println(itr.next());
+       }
+      
+       System.out.println("\nsize : " + upq.size());
+       System.out.println("Removed : " + upq.remove()); 
+       System.out.println("Peek : " + upq.peek());   
+       System.out.println("empty: " + upq.isEmpty());
+   }
+}
